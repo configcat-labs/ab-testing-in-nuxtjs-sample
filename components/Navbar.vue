b04bbbfc30e4bcb61124657ade6ae78d2e422418
Chavez Harris <codedbychavez@gmail>
2022-08-12 12:10:03 -0300

    fixed typo

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -15,7 +15,7 @@
           <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Home</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Women Clothing</a>
+          <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Women's Clothing</a>
         </li>
         <li>
           <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Mens Clothing</a>
